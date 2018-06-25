eeb57bcc942f407151b71bfab528e817c6806c74
Saranya <ressellak@bitbucket.org>
2018-06-25 17:51:22 +0530

    My first changes

--- .fr-8g0YNr/get-me-through/server/db/db.js
+++
@@ -1,6 +0,0 @@
-let mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/GetMeThrough',{ useMongoClient: true });
-
-module.exports = {mongoose};
